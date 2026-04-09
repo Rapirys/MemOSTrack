cae664b710e12349f2af22fa56a3c664cda81412
illia <ilyarotan@gmail.com>
2026-04-09 23:31:21 +0200

    Remote testing setup

--- install.sh
+++ install.sh
@@ -3,101 +3,98 @@ conda install -y pytorch==1.12.1 torchvision==0.13.1 torchaudio==0.12.1 cudatool
 
 echo ""
 echo ""
-echo "****************** Installing yaml ******************"
-pip install PyYAML
-
-echo ""
-echo ""
-echo "****************** Installing easydict ******************"
-pip install easydict
-
-echo ""
-echo ""
-echo "****************** Installing cython ******************"
-pip install cython
-
-echo ""
-echo ""
-echo "****************** Installing opencv-python ******************"
-pip install opencv-python
-
-echo ""
-echo ""
-echo "****************** Installing pandas ******************"
-pip install pandas
-
-echo ""
-echo ""
 echo "****************** Installing tqdm ******************"
 conda install -y tqdm
 
 echo ""
 echo ""
+echo "****************** Installing lmdb ******************"
+conda install -y -c conda-forge python-lmdb
+
+echo ""
+echo ""
+echo "****************** Installing yaml ******************"
+python -m pip install PyYAML
+
+echo ""
+echo ""
+echo "****************** Installing easydict ******************"
+python -m pip install easydict
+
+echo ""
+echo ""
+echo "****************** Installing cython ******************"
+python -m pip install cython
+
+echo ""
+echo ""
+echo "****************** Installing opencv-python ******************"
+python -m pip install opencv-python
+
+echo ""
+echo ""
+echo "****************** Installing pandas ******************"
+python -m pip install pandas
+
+echo ""
+echo ""
 echo "****************** Installing coco toolkit ******************"
-pip install pycocotools
+python -m pip install pycocotools
 
 echo ""
 echo ""
 echo "****************** Installing jpeg4py python wrapper ******************"
-pip install jpeg4py
+python -m pip install jpeg4py
 
 echo ""
 echo ""
 echo "****************** Installing tensorboard ******************"
-pip install tb-nightly
+python -m pip install tb-nightly
 
 echo ""
 echo ""
 echo "****************** Installing tikzplotlib ******************"
-pip install tikzplotlib
+python -m pip install tikzplotlib
 
 echo ""
 echo ""
 echo "****************** Installing thop tool for FLOPs and Params computing ******************"
-pip install thop-0.0.31.post2005241907
+python -m pip install thop==0.0.31.post2005241907
 
 echo ""
 echo ""
 echo "****************** Installing colorama ******************"
-pip install colorama
-
-echo ""
-echo ""
-echo "****************** Installing lmdb ******************"
-pip install lmdb
+python -m pip install colorama
 
 echo ""
 echo ""
 echo "****************** Installing scipy ******************"
-pip install scipy
+python -m pip install scipy
 
 echo ""
 echo ""
 echo "****************** Installing visdom ******************"
-pip install visdom
-
+python -m pip install visdom
 
 echo ""
 echo ""
 echo "****************** Installing tensorboardX ******************"
-pip install tensorboardX
-
+python -m pip install tensorboardX
 
 echo ""
 echo ""
 echo "****************** Downgrade setuptools ******************"
-pip install setuptools==59.5.0
-
+python -m pip install setuptools==59.5.0
 
 echo ""
 echo ""
 echo "****************** Installing wandb ******************"
-pip install wandb
+python -m pip install wandb
 
 echo ""
 echo ""
 echo "****************** Installing timm ******************"
-pip install timm==0.6.13
+python -m pip install timm==0.6.13
 
 echo ""
 echo ""
